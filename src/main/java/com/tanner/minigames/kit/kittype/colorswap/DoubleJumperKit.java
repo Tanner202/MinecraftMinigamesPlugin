cab580f74c3cb3e6be77a434021e78e417606c39
Tanner <tannerelephant@gmail.com>
2025-06-23 20:00:05 -0700

    add double jump cooldown to double jumper kit

--- src/main/java/com/tanner/minigames/kit/kittype/colorswap/DoubleJumperKit.java
+++ src/main/java/com/tanner/minigames/kit/kittype/colorswap/DoubleJumperKit.java
@@ -1,9 +1,10 @@
 package com.tanner.minigames.kit.kittype.colorswap;
 
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
 import com.tanner.minigames.Minigames;
 import com.tanner.minigames.kit.ColorSwapKitType;
 import com.tanner.minigames.kit.Kit;
-import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.GameMode;
 import org.bukkit.Sound;
@@ -13,10 +14,13 @@ import org.bukkit.event.player.PlayerToggleFlightEvent;
 import org.bukkit.util.Vector;
 
 import java.util.UUID;
+import java.util.concurrent.TimeUnit;
 
 public class DoubleJumperKit extends Kit {
 
     private Player kitOwner;
+    private int doubleJumpCooldown = 3;
+    private Cache<UUID, Long> cooldown = CacheBuilder.newBuilder().expireAfterWrite(doubleJumpCooldown, TimeUnit.SECONDS).build();
     private int remainingUses = 3;
     private float playerDoubleJumpPower = 1f;
     private float forwardPower = 1f;
@@ -45,11 +49,18 @@ public class DoubleJumperKit extends Kit {
     public void onPlayerToggleFlight(PlayerToggleFlightEvent e) {
         Player player = e.getPlayer();
 
+        if (!cooldown.asMap().containsKey(player.getUniqueId()) && remainingUses != 0) {
+            cooldown.put(player.getUniqueId(), TimeUnit.MILLISECONDS.toSeconds(System.currentTimeMillis()) + doubleJumpCooldown);
+        } else {
+            long timeRemaining = cooldown.asMap().get(player.getUniqueId()) - TimeUnit.MILLISECONDS.toSeconds(System.currentTimeMillis());
+            player.sendMessage(ChatColor.RED + "You must wait " + timeRemaining + " second(s) before using double jump!");
+            e.setCancelled(true);
+            return;
+        }
+
         if (e.getPlayer().equals(kitOwner)) {
             e.setCancelled(true);
             if (remainingUses > 0) {
-                e.setCancelled(true);
-
                 Vector playerDirection = player.getLocation().getDirection();
                 Vector doubleJumpVector = new Vector(playerDirection.getX() * forwardPower, playerDoubleJumpPower,
                         playerDirection.getZ() * forwardPower);
