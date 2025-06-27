787f73805915ab405af3567a44568c9c83c39180
Tanner <tannerelephant@gmail.com>
2025-06-27 07:16:21 -0700

    add cool visuals to double jumper kit

--- src/main/java/com/tanner/minigames/kit/kittype/colorswap/DoubleJumperKit.java
+++ src/main/java/com/tanner/minigames/kit/kittype/colorswap/DoubleJumperKit.java
@@ -5,12 +5,16 @@ import com.google.common.cache.CacheBuilder;
 import com.tanner.minigames.Minigames;
 import com.tanner.minigames.kit.ColorSwapKitType;
 import com.tanner.minigames.kit.Kit;
+import net.md_5.bungee.api.ChatMessageType;
+import net.md_5.bungee.api.chat.TextComponent;
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.GameMode;
 import org.bukkit.Sound;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.player.PlayerToggleFlightEvent;
+import org.bukkit.scheduler.BukkitTask;
 import org.bukkit.util.Vector;
 
 import java.util.UUID;
@@ -51,6 +55,7 @@ public class DoubleJumperKit extends Kit {
 
         if (!cooldown.asMap().containsKey(player.getUniqueId()) && remainingUses != 0) {
             cooldown.put(player.getUniqueId(), TimeUnit.MILLISECONDS.toSeconds(System.currentTimeMillis()) + doubleJumpCooldown);
+            setExpBarCooldownVisual(player);
         } else {
             long timeRemaining = cooldown.asMap().get(player.getUniqueId()) - TimeUnit.MILLISECONDS.toSeconds(System.currentTimeMillis());
             player.sendMessage(ChatColor.RED + "You must wait " + timeRemaining + " second(s) before using double jump!");
@@ -69,9 +74,27 @@ public class DoubleJumperKit extends Kit {
                 player.playSound(player.getLocation(), Sound.ENTITY_ENDER_DRAGON_FLAP, 1f, 1f);
                 remainingUses--;
                 kitOwner.sendMessage(ChatColor.GREEN + "Double Jump used! " + remainingUses + " use" + (remainingUses == 1 ? "" : "s") + " remaining!");
+                if (remainingUses >= 1) {
+                    Bukkit.getScheduler().runTaskLater(minigames, () -> player.spigot().sendMessage(ChatMessageType.ACTION_BAR,
+                            TextComponent.fromLegacy("§a§lDouble Jump is ready!")), doubleJumpCooldown * 20);
+                } else {
+                    player.spigot().sendMessage(ChatMessageType.ACTION_BAR,
+                            TextComponent.fromLegacy("§4§lYou are out of usages!"));
+                }
             } else {
                 kitOwner.sendMessage(ChatColor.RED + "You are out of usages!");
             }
         }
+    }
+
+    private void setExpBarCooldownVisual(Player player) {
+        BukkitTask task = Bukkit.getScheduler().runTaskTimer(minigames, () -> {
+            float timeRemaining = TimeUnit.SECONDS.toMillis(cooldown.asMap().get(player.getUniqueId())) - System.currentTimeMillis();
+            player.setExp(timeRemaining/TimeUnit.SECONDS.toMillis(doubleJumpCooldown));
+        }, 0, 1);
+        Bukkit.getScheduler().runTaskLater(minigames, () -> {
+            task.cancel();
+            player.setExp(0);
+        }, doubleJumpCooldown * 20L);
     }
 }
