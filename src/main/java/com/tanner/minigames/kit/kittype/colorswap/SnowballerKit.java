6148245b25f99fd2710004cd4603c61730756e40
Tanner <tannerelephant@gmail.com>
2025-07-23 08:43:58 +0200

    colorswap: adjust snowballer kit to have a max snowball amount and bind snowballs to first player slot

--- src/main/java/com/tanner/minigames/kit/kittype/colorswap/SnowballerKit.java
+++ src/main/java/com/tanner/minigames/kit/kittype/colorswap/SnowballerKit.java
@@ -21,6 +21,9 @@ public class SnowballerKit extends Kit {
 
     private long snowballCooldown = 8;
     private float knockbackMultiplier = 2;
+    private int snowballAddAmount = 4;
+    private int snowballSlot = 0;
+    private int maxSnowballAmount = 16;
 
     public SnowballerKit(Minigames minigames, UUID uuid) {
         super(minigames, ColorSwapKitType.BALLER, uuid);
@@ -29,7 +32,9 @@ public class SnowballerKit extends Kit {
     @Override
     public void onStart(Player player) {
         snowballTaskID = Bukkit.getScheduler().scheduleSyncRepeatingTask(minigames, () -> {
-            giveSnowballs(player);
+            if ((!hasSnowballs(player)) || (hasSnowballs(player) && player.getInventory().getItem(snowballSlot).getAmount() < maxSnowballAmount)) {
+                giveSnowballs(player);
+            }
         },0, snowballCooldown * 20);
     }
 
@@ -39,11 +44,24 @@ public class SnowballerKit extends Kit {
     }
 
     private void giveSnowballs(Player player) {
-        ItemStack snowballs = new ItemStack(Material.SNOWBALL, 4);
+        ItemStack snowballs = new ItemStack(Material.SNOWBALL, snowballAddAmount);
+        if (hasSnowballs(player)
+                && player.getInventory().getItem(snowballSlot).getAmount() + snowballs.getAmount() > maxSnowballAmount) {
+            snowballs.setAmount(maxSnowballAmount - player.getInventory().getItem(snowballSlot).getAmount());
+        }
         ItemMeta itemMeta = snowballs.getItemMeta();
         itemMeta.setDisplayName(ChatColor.AQUA + "Super Special Snowballs");
         snowballs.setItemMeta(itemMeta);
-        player.getInventory().addItem(snowballs);
+
+        if (player.getInventory().getItem(snowballSlot) != null && !player.getInventory().getItem(snowballSlot).getType().equals(Material.SNOWBALL)) {
+            player.getInventory().setItem(snowballSlot, snowballs);
+        } else {
+            player.getInventory().addItem(snowballs);
+        }
+    }
+
+    private boolean hasSnowballs(Player player) {
+        return player.getInventory().getItem(snowballSlot) != null && player.getInventory().getItem(snowballSlot).getType().equals(Material.SNOWBALL);
     }
 
     @EventHandler
