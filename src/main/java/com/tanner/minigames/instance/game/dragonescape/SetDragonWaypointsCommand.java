e6fd8109492cb2b5e6ae11b7ffc65190e9dcc6a5
Tanner <tannerelephant@gmail.com>
2025-07-24 10:43:22 +0200

    dragon escape: improve setting dragon waypoints

--- src/main/java/com/tanner/minigames/instance/game/dragonescape/SetDragonWaypointsCommand.java
+++ src/main/java/com/tanner/minigames/instance/game/dragonescape/SetDragonWaypointsCommand.java
@@ -1,10 +1,13 @@
 package com.tanner.minigames.instance.game.dragonescape;
 
 import com.tanner.minigames.Minigames;
+import net.md_5.bungee.api.ChatMessageType;
+import net.md_5.bungee.api.chat.TextComponent;
 import org.bukkit.*;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.ArmorStand;
 import org.bukkit.entity.Entity;
@@ -12,25 +15,37 @@ import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.player.PlayerInteractAtEntityEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.scheduler.BukkitTask;
 
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
 
 public class SetDragonWaypointsCommand implements CommandExecutor, Listener {
+
+    private Minigames minigames;
 
     private List<UUID> activeWaypointEditors = new ArrayList<>();
     private YamlConfiguration ymlFile;
     private File file;
     private List<ArmorStand> waypoints = new ArrayList<>();
     private World world;
+    private int selectedWaypointIndex = -1;
+
+    BukkitTask selectedWaypointMessage;
 
     public SetDragonWaypointsCommand(Minigames minigames) {
+        this.minigames = minigames;
         file = minigames.getFileManager().getFile(Paths.get("dragon_escape/dragon_locations.yml"));
         ymlFile = YamlConfiguration.loadConfiguration(file);
         world = Bukkit.getWorld(ymlFile.getString("dragon-spawn.world"));
@@ -44,16 +59,18 @@ public class SetDragonWaypointsCommand implements CommandExecutor, Listener {
                     ymlFile.getDouble(prefix + key + ".y"),
                     ymlFile.getDouble(prefix + key + ".z"));
             boolean foundArmorStand = false;
-            for (Entity entity : world.getNearbyEntities(loc, 2, 2, 2)) {
+            for (Entity entity : world.getNearbyEntities(loc, 0.5, 0.5, 0.5)) {
                 if (entity.getType().equals(EntityType.ARMOR_STAND)) {
                     waypoints.add((ArmorStand) entity);
                     foundArmorStand = true;
                 }
             }
             if (!foundArmorStand) {
-                spawnArmorStand(loc);
+                addWaypoint(loc);
             }
         }
+
+        selectedWaypointIndex = waypoints.size() - 1;
     }
 
     @Override
@@ -63,11 +80,26 @@ public class SetDragonWaypointsCommand implements CommandExecutor, Listener {
             if (!activeWaypointEditors.contains(player.getUniqueId())) {
                 activeWaypointEditors.add(player.getUniqueId());
                 player.sendMessage(ChatColor.GREEN + "Enabled Dragon Waypoint Editing");
+                giveWaypointItems(player);
                 setWaypointsVisible(true);
+                selectedWaypointMessage = Bukkit.getScheduler().runTaskTimer(minigames, () -> {
+                    if (selectedWaypointIndex >= 0) {
+                        player.spigot().sendMessage(ChatMessageType.ACTION_BAR, TextComponent.fromLegacy("§a§lSelected Waypoint: " + selectedWaypointIndex));
+                    } else {
+                        player.spigot().sendMessage(ChatMessageType.ACTION_BAR, TextComponent.fromLegacy("§a§lSelected Waypoint: None"));
+                    }
+                }, 0, 40);
             } else {
                 activeWaypointEditors.remove(player.getUniqueId());
                 player.sendMessage(ChatColor.RED + "Disabled Dragon Waypoint Editing");
+                player.getInventory().clear();
                 setWaypointsVisible(false);
+                selectedWaypointMessage.cancel();
+                try {
+                    saveWaypointsToYAML();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
             }
         }
         return false;
@@ -79,68 +111,169 @@ public class SetDragonWaypointsCommand implements CommandExecutor, Listener {
         }
     }
 
+    private void giveWaypointItems(Player player) {
+        Inventory inv = player.getInventory();
+
+        ItemStack addWaypointItem = new ItemStack(Material.ARMOR_STAND);
+        ItemMeta addWaypointMeta = addWaypointItem.getItemMeta();
+        addWaypointMeta.setItemName(ChatColor.GREEN + "Waypoint Creator");
+        addWaypointMeta.setLore(Arrays.asList("Right click a block to add a waypoint after the current waypoint"));
+        addWaypointItem.setItemMeta(addWaypointMeta);
+
+        ItemStack removeWaypointItem = new ItemStack(Material.IRON_AXE);
+        ItemMeta removeWaypointMeta = removeWaypointItem.getItemMeta();
+        removeWaypointMeta.setItemName(ChatColor.RED + "Waypoint Remover");
+        removeWaypointMeta.setLore(Arrays.asList("Right click a waypoint to remove it!"));
+        removeWaypointItem.setItemMeta(removeWaypointMeta);
+
+        ItemStack waypointSelectorItem = new ItemStack(Material.STICK);
+        ItemMeta waypointSelectorMeta = waypointSelectorItem.getItemMeta();
+        waypointSelectorMeta.setItemName(ChatColor.AQUA + "Waypoint Selector");
+        waypointSelectorMeta.setLore(Arrays.asList("Right click a waypoint to select it!"));
+        waypointSelectorItem.setItemMeta(waypointSelectorMeta);
+
+        ItemStack waypointTeleporterItem = new ItemStack(Material.COMPASS);
+        ItemMeta waypointTeleporterMeta = waypointTeleporterItem.getItemMeta();
+        waypointTeleporterMeta.setItemName(ChatColor.DARK_PURPLE + "Waypoint Teleporter (Right Click)");
+        waypointTeleporterItem.setItemMeta(waypointTeleporterMeta);
+
+        ItemStack saveItem = new ItemStack(Material.PAPER);
+        ItemMeta saveMeta = saveItem.getItemMeta();
+        saveMeta.setItemName(ChatColor.GREEN + "Save");
+        saveItem.setItemMeta(saveMeta);
+
+        inv.setItem(0, addWaypointItem);
+        inv.setItem(1, removeWaypointItem);
+        inv.setItem(2, waypointSelectorItem);
+        inv.setItem(4, waypointTeleporterItem);
+        inv.setItem(8, saveItem);
+    }
+
     @EventHandler
-    public void onPlayerInteract(PlayerInteractEvent e) throws IOException {
+    public void onPlayerInteract(PlayerInteractEvent e) {
         Player player = e.getPlayer();
-        if (activeWaypointEditors.contains(player.getUniqueId()) && e.getClickedBlock() != null && e.getItem() != null
-                && e.getItem().getType().equals(Material.ARMOR_STAND)) {
-            e.setCancelled(true);
-            int newKey;
-            if (!ymlFile.getConfigurationSection("target-locations").getKeys(false).isEmpty()) {
-                String[] keys = ymlFile.getConfigurationSection("target-locations").getKeys(false).toArray(new String[0]);
-                newKey = Integer.parseInt(keys[keys.length - 1]) + 1;
-            } else {
-                newKey = 0;
+        if (activeWaypointEditors.contains(player.getUniqueId())) {
+            if (e.getClickedBlock() != null && e.getItem() != null
+                    && e.getItem().getType().equals(Material.ARMOR_STAND)) {
+                e.setCancelled(true);
+                ArmorStand armorStand = addWaypoint(e.getClickedBlock().getLocation());
+                Location loc = armorStand.getLocation();
+                player.sendMessage(ChatColor.GREEN + "Set waypoint at (" + loc.getX() + ", " + loc.getY() + ", " + loc.getZ() + ")");
+                player.playSound(player.getLocation(), Sound.ENTITY_EXPERIENCE_ORB_PICKUP, 1, 1);
+                selectedWaypointIndex++;
+                player.spigot().sendMessage(ChatMessageType.ACTION_BAR, TextComponent.fromLegacy("§a§lSelected Waypoint: " + selectedWaypointIndex));
+            } else if (e.getItem().getType().equals(Material.COMPASS)) {
+                Inventory gui = Bukkit.createInventory(player, 27, ChatColor.DARK_PURPLE.toString() + ChatColor.BOLD + "Waypoint Teleporter");
+                for (int i = 0; i < waypoints.size(); i++) {
+                    ItemStack stand = new ItemStack(Material.ARMOR_STAND);
+                    ItemMeta standMeta = stand.getItemMeta();
+                    standMeta.setItemName(ChatColor.GREEN + "Waypoint " + i);
+                    Location loc = waypoints.get(i).getLocation();
+                    standMeta.setLore(Arrays.asList("X: " + loc.getX(), "Y: " + loc.getY(), "Z: " + loc.getZ()));
+                    stand.setItemMeta(standMeta);
+                    gui.setItem(i, stand);
+                }
+                player.openInventory(gui);
+            } else if (player.getInventory().getItemInMainHand().getType().equals(Material.PAPER)) {
+                try {
+                    saveWaypointsToYAML();
+                } catch (IOException exc) {
+                    exc.printStackTrace();
+                }
+                player.sendMessage(ChatColor.GREEN + "Saved Waypoints to YAML!");
             }
-            ArmorStand armorStand = spawnArmorStand(e.getClickedBlock().getLocation());
-            String prefix = "target-locations.";
-            Location loc = armorStand.getLocation();
-            double x = loc.getX();
-            double y = loc.getY();
-            double z = loc.getZ();
-            ymlFile.set(prefix + newKey + ".x", x);
-            ymlFile.set(prefix + newKey + ".y", y);
-            ymlFile.set(prefix + newKey + ".z", z);
-            ymlFile.save(file);
-            player.sendMessage(ChatColor.GREEN + "Set waypoint at (" + x + ", " + y + ", " + z + ")");
-            player.playSound(player.getLocation(), Sound.ENTITY_EXPERIENCE_ORB_PICKUP, 1, 1);
         }
     }
 
-    private ArmorStand spawnArmorStand(Location loc) {
+    private void saveWaypointsToYAML() throws IOException {
+        ConfigurationSection waypointsSection = ymlFile.getConfigurationSection("target-locations");
+        if (waypointsSection != null) {
+            for (String key : waypointsSection.getKeys(false)) {
+                waypointsSection.set(key, null);
+            }
+        } else {
+            ymlFile.createSection("target-locations"); // ensure it exists if not already
+        }
+
+        int waypointIndex = 0;
+        for (ArmorStand stand : waypoints) {
+            String prefix = "target-locations.";
+            Location loc = stand.getLocation();
+            double x = loc.getX();
+            double y = loc.getY();
+            double z = loc.getZ();
+            ymlFile.set(prefix + waypointIndex + ".x", x);
+            ymlFile.set(prefix + waypointIndex + ".y", y);
+            ymlFile.set(prefix + waypointIndex + ".z", z);
+            waypointIndex++;
+        }
+        ymlFile.save(file);
+    }
+
+    private ArmorStand addWaypoint(Location loc) {
         ArmorStand armorStand = (ArmorStand) loc.getWorld().spawnEntity(loc.add(0, 1, 0), EntityType.ARMOR_STAND);
         if (activeWaypointEditors.isEmpty()) {
             armorStand.setInvisible(true);
         }
         armorStand.setInvulnerable(true);
-        waypoints.add(armorStand);
+        if (selectedWaypointIndex + 1 > waypoints.size() - 1) {
+            waypoints.add(armorStand);
+        } else {
+            waypoints.add(selectedWaypointIndex + 1, armorStand);
+        }
         return armorStand;
     }
 
+    private void removeWaypoint(Entity stand, Location loc) {
+        waypoints.remove(stand);
+        stand.remove();
+
+        for (String key : ymlFile.getConfigurationSection("target-locations").getKeys(false)) {
+            String prefix = "target-locations.";
+            Location fileLoc = new Location(world,
+                    ymlFile.getDouble(prefix + key + ".x"),
+                    ymlFile.getDouble(prefix + key + ".y"),
+                    ymlFile.getDouble(prefix + key + ".z"));
+            if (fileLoc.equals(loc)) {
+                ymlFile.set(prefix + key, null);
+            }
+        }
+    }
+
     @EventHandler
-    public void onPlayerInteractAtEntity(PlayerInteractAtEntityEvent e) throws IOException {
+    public void onInventoryClickEvent(InventoryClickEvent e) {
+        if (ChatColor.translateAlternateColorCodes('&', e.getView().getTitle()).equals(ChatColor.DARK_PURPLE.toString() + ChatColor.BOLD + "Waypoint Teleporter")
+        && e.getCurrentItem() != null) {
+            Player player = (Player) e.getWhoClicked();
+            player.teleport(waypoints.get(e.getRawSlot()).getLocation());
+            e.setCancelled(true);
+        }
+    }
+
+    @EventHandler
+    public void onPlayerInteractAtEntity(PlayerInteractAtEntityEvent e) {
         Player player = e.getPlayer();
         Entity entity = e.getRightClicked();
         Location loc = entity.getLocation();
         if (activeWaypointEditors.contains(player.getUniqueId()) && entity.getType().equals(EntityType.ARMOR_STAND) &&
                 waypoints.contains((ArmorStand) entity)) {
-            waypoints.remove(entity);
-            entity.remove();
 
-            for (String key : ymlFile.getConfigurationSection("target-locations").getKeys(false)) {
-                String prefix = "target-locations.";
-                Location fileLoc = new Location(world,
-                        ymlFile.getDouble(prefix + key + ".x"),
-                        ymlFile.getDouble(prefix + key + ".y"),
-                        ymlFile.getDouble(prefix + key + ".z"));
-                if (fileLoc.equals(loc)) {
-                    ymlFile.set(prefix + key, null);
-                    ymlFile.save(file);
+            if (player.getInventory().getItemInMainHand().getType().equals(Material.IRON_AXE)) {
+                removeWaypoint(entity, loc);
+
+                player.sendMessage(ChatColor.RED + "Removed waypoint at (" + loc.getX() + ", " + loc.getY() + ", " + loc.getZ() + ")");
+                player.playSound(player.getLocation(), Sound.BLOCK_STONE_BREAK, 1, 1);
+                selectedWaypointIndex--;
+                player.spigot().sendMessage(ChatMessageType.ACTION_BAR, TextComponent.fromLegacy("§a§lSelected Waypoint: " + selectedWaypointIndex));
+            } else if ((player.getInventory().getItemInMainHand().getType().equals(Material.STICK))) {
+                player.sendMessage(ChatColor.GREEN + "Selected waypoint");
+                for (ArmorStand stand : waypoints) {
+                    if (stand.getLocation().equals(entity.getLocation())) {
+                        selectedWaypointIndex = waypoints.indexOf(stand);
+                    }
                 }
+                player.spigot().sendMessage(ChatMessageType.ACTION_BAR, TextComponent.fromLegacy("§a§lSelected Waypoint: " + selectedWaypointIndex));
             }
-
-            player.sendMessage(ChatColor.RED + "Removed waypoint at (" + loc.getX() + ", " + loc.getY() + ", " + loc.getZ() + ")");
-            player.playSound(player.getLocation(), Sound.BLOCK_STONE_BREAK, 1, 1);
         }
     }
 }
